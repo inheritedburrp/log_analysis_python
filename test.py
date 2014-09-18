b77010f98233af120a1b7df0f8774688b472623f
Anuj <anuj.want002@gmail.com>
2014-09-18 05:41:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,125 +7,98 @@ def break_words(stuff):
 
 def pop_first_word(words):
 	word = words.pop(0)
-	dec = word.split('.')
-	time=dec[0]
-	duration=dec[1]
-	return time, duration
+	return word
 
-def pop_sec_word(words):
+def pop_word(words):
+	word = words.pop(4)
+	return word
+	
+def pop_se_word(words):
 	word = words.pop(5)
-	return word
-
-def pop_third_word(words):
-	word = words.pop(6)
-	return word
-
-def pop_fourth_word(words):
-	word = words.pop(7)
-	return word
-
-def pop_fifth_word(words):
-	word = words.pop(8)
-	return word
-
-def pop_sixth_word(words):
-	word = words.pop(9)
-	return word
-def pop_seventh_word(words):
-	word = words.pop(10)
-	return word
-
-def pop_eighth_word(words):
-	word = words.pop(11)
-	return word
-
-def pop_ninth_word(words):
-	word = words.pop(12)
-	return word
-
-def pop_tenth_word(words):
-	word = words.pop(13)
 	return word
 
 
 log= open(filename)
 num_lines = sum(1 for lines in log)
 log.seek(0)
-p=log.readlines()[0]
-sentence=break_words(p)
-print sentence
+a=0
+
+
+
+while a < num_lines:
+
+	
+	p=log.readlines()[a]
+	sentence=break_words(p)
 
 
 
 
-q,r=pop_first_word(sentence)
-time=[]
-duration=[]
-time.append(q)
-duration.append(r)
-print "Array of Time", time
-print "Array of Duration", duration
-
-
-'''
-
-
-r=pop_sec_word(sentence)
-duration=[]
-duration.append(r)
-print "array of Duration", duration
-
-
-
-s=pop_sec_word(sentence)
-remotehost=[]
-duration.append(s)
-print "array of remotehost", remotehost
-
-'''
-'''
-r=pop_sec_word(sentence)
-duration=[]
-duration.append(r)
-print "array of Duration", duration
-
-
-
-
-r=pop_sec_word(sentence)
-duration=[]
-duration.append(r)
-print "array of Duration", duration
-
-
-
-
-r=pop_sec_word(sentence)
-duration=[]
-duration.append(r)
-print "array of Duration", duration
+	q=pop_first_word(sentence)
+	time=[]
+	time.append(q)
+	print "Array of Time", time
 
 
 
 
 
-r=pop_sec_word(sentence)
-duration=[]
-duration.append(r)
-print "array of Duration", duration
+	r=pop_word(sentence)
+	duration=[]
+	duration.append(r)
+	print "array of Duration", duration
+
+
+
+	s=pop_word(sentence)
+	remotehost=[]
+	remotehost.append(s)
+	print "array of remotehost", remotehost
+
+
+
+	t=pop_word(sentence)
+	code_status=[]
+	code_status.append(t)
+	print "array of code_status", code_status
 
 
 
 
-r=pop_sec_word(sentence)
-duration=[]
-duration.append(r)
-print "array of Duration", duration
+	u=pop_word(sentence)
+	bytes=[]
+	bytes.append(u)
+	print "array of Page Size in bytes", bytes
+
+
+
+	v=pop_word(sentence)
+	method=[]
+	method.append(v)
+	print "array of Request method", method
 
 
 
 
-r=pop_sec_word(sentence)
-duration=[]
-duration.append(r)
-print "array of Duration", duration  '''
+	w=pop_word(sentence)
+	url=[]
+	url.append(w)
+	print "array of URL", url
+
+
+
+	x=pop_se_word(sentence)
+	hierarchy=[]
+	hierarchy.append(x)
+	print "array of Hierarchy code", hierarchy
+
+
+
+
+	y=pop_se_word(sentence)
+	type=[]
+	type.append(y)
+	print "array of Type", type
+	
+	
+	a = a + 1
