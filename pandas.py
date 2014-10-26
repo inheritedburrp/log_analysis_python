3bf86d7c9b5c58b15e52fdd96feae48652786d6d
Anuj <anuj.want002@gmail.com>
2014-10-26 19:28:48 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -10,3 +10,24 @@ class RedisLogger(logging.Handler):
     self.port = port
     self.db = db
     self.r = Redis(host, port=port, db=db)
+  def emit(self, record):
+    try:
+      self.r.lpush(self.list_name, self.format(record))
+    except:
+      try:
+        self.r = Redis(self.host)
+        self.r.lpush(self.list_name, self.format(record))
+      except:
+        print "Failed to log on Redis queue"
+
+if __name__ == "__main__":
+  import logging
+  import logging.config
+
+  logging.config.fileConfig("logging.conf.example")
+  
+  logger = logging.getLogger("root")
+ 
+  print "Logging a message, both to a file (access.log) and also lpush the same string to a redis list (q:demo in db=10)"
+  print "These are defined in the example logging.conf.example file for reference"
+  logger.info("Testing the dual logging abilities - to a file and to a redis list")
